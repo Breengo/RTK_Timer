bfb24e66c958f84ac7198b66fc9dfe36e0812921
Greengo <vipser988@gmail.com>
2024-01-25 19:42:26 +0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [sec, setSec] = React.useState(0);
+  const [mins, setMins] = React.useState(0);
+  const [hours, setHours] = React.useState(0);
+  const [isWorking, setIsWorking] = React.useState(false);
+  const timerRef = React.useRef<ReturnType<typeof setInterval>>();
+  const remainingTimeRef = React.useRef(0);
+
+  function changeSeconds(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value) {
+      let value = parseInt(e.target.value);
+      if (value < 60) setSec(value);
+    } else setSec(0);
+  }
+
+  function changeMins(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value) {
+      let value = parseInt(e.target.value);
+      if (value < 60) setMins(value);
+    } else setMins(0);
+  }
+
+  function makeTick() {
+    if (remainingTimeRef.current <= 0) {
+      setIsWorking(false);
+      alert("Время истекло");
+      return clearInterval(timerRef.current);
+    }
+    const now = new Date().getTime();
+    remainingTimeRef.current -= 1;
+    let time = Math.floor(remainingTimeRef.current);
+    const rHours = Math.floor(time / 3600);
+    time = time % 3600;
+    const rMins = Math.floor(time / 60);
+    time = time % 60;
+    setHours(rHours);
+    setMins(rMins);
+    setSec(time);
+  }
+
+  function onClick() {
+    if (!isWorking) {
+      setIsWorking(true);
+      remainingTimeRef.current = sec + mins * 60 + hours * 3600;
+      timerRef.current = setInterval(makeTick, 1000);
+    } else {
+      setIsWorking(false);
+      clearInterval(timerRef.current);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-white bg-neutral-800 w-full h-screen flex flex-col items-center justify-center">
+      <div className="flex items-center gap-2">
+        <input
+          disabled={isWorking}
+          placeholder="00"
+          className="bg-transparent w-40 p-2 rounded-md text-5xl text-center outline-none"
+          type="text"
+          onChange={(e) =>
+            setHours(parseInt(e.target.value ? e.target.value : "0"))
+          }
+          value={hours}
+        />
+
+        <input
+          disabled={isWorking}
+          placeholder="00"
+          className=" bg-transparent w-20 p-2 rounded-md text-5xl text-center outline-none"
+          type="text"
+          onChange={changeMins}
+          value={mins}
+        />
+
+        <input
+          disabled={isWorking}
+          placeholder="00"
+          className="bg-transparent w-20 p-2 rounded-md text-5xl text-center outline-none"
+          type="text"
+          onChange={changeSeconds}
+          value={sec}
+        />
+      </div>
+      <button
+        onClick={onClick}
+        className="mt-8 border rounded-md px-8 py-2 transition-all hover:bg-neutral-400"
+      >
+        {isWorking ? "Отменить" : "Запустить"}
+      </button>
     </div>
   );
 }
